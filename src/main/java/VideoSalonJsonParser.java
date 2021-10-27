91dfa13a8d7865e311fd404e6a875523b0180084
dzianis.piatrovich <dzianis.piatrovich@gmail.com>
2021-10-27 13:13:44 +0300

    delete main method from VideoSalonJsonParser

--- src/main/java/VideoSalonJsonParser.java
+++ src/main/java/VideoSalonJsonParser.java
@@ -9,36 +9,6 @@ import java.util.Scanner;
 
 public class VideoSalonJsonParser {
 
-    public static void main(String[] args) throws IOException {
-
-
-        VideoTape videoTape = new VideoTape("Alice in Wonderland", true, "Igor", LocalDateTime.now());
-        VideoTape videoTape2 = new VideoTape("Resident Evil", true, "Igor", LocalDateTime.now());
-        VideoTape videoTape3 = new VideoTape("Batman. The beginning", true, "Igor", LocalDateTime.now());
-        VideoTape videoTape4 = new VideoTape("The amazing Spider-Man", true, "Igor", LocalDateTime.now());
-
-        List<VideoTape> videoTapeList = new ArrayList<>();
-        videoTapeList.add(videoTape);
-        videoTapeList.add(videoTape2);
-        videoTapeList.add(videoTape3);
-        videoTapeList.add(videoTape4);
-
-        StringWriter writer = new StringWriter();
-        ObjectMapper mapper = new ObjectMapper();
-
-        mapper.writeValue(writer, videoTapeList);
-        String result =  writer.toString();
-        System.out.println(result);
-        System.out.println("-----");
-
-//        Scanner scanner = new Scanner(new File("src/main/java/VideoTapes.txt"));
-//        String jsonArray = scanner.nextLine();
-        List<VideoTape> jVideoTapeList = mapper.readValue(result, new TypeReference<List<VideoTape>>(){});
-        System.out.println(jVideoTapeList.toString());
-        System.out.println(jVideoTapeList.get(0));
-
-       }
-
     ObjectMapper mapper = new ObjectMapper();
     StringWriter writer = new StringWriter();
 
